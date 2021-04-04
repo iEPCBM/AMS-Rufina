1af6a06bef0098bb2e79a0481e050e6cb5908377
iEPCBM <rishatk87@gmail.com>
2021-04-04 14:56:44 +0300

    Update

--- widgets/chatactionbutton.cpp
+++ widgets/chatactionbutton.cpp
@@ -1,6 +1,28 @@
 #include "chatactionbutton.h"
 
-ChatActionButton::ChatActionButton()
+ChatActionButton::ChatActionButton(QString &text, VkChat *chat, QWidget *parent):QPushButton(text, parent)
 {
+    m_chat = chat;
+    connect(this, SIGNAL(clicked()), this, SLOT(doChatAction()));
+}
 
+ChatActionButton::~ChatActionButton()
+{
+    delete m_chat;
+    m_chat = NULL;
+}
+
+VkChat *ChatActionButton::chat() const
+{
+    return m_chat;
+}
+
+void ChatActionButton::setChat(VkChat *chat)
+{
+    m_chat = chat;
+}
+
+void ChatActionButton::doChatAction()
+{
+    emit cabtClicked(m_chat);
 }
--- widgets/chatactionbutton.h
+++ widgets/chatactionbutton.h
@@ -3,11 +3,25 @@
 
 #include <QPushButton>
 #include <QWidget>
+#include "vkApi/vkchat.h"
 
 class ChatActionButton : public QPushButton
 {
+    Q_OBJECT
 public:
-    ChatActionButton();
+    explicit ChatActionButton(QString &text, VkChat *chat = nullptr, QWidget *parent = nullptr);
+    ~ChatActionButton();
+    VkChat *chat() const;
+    void setChat(VkChat *chat);
+
+public slots:
+    void doChatAction();
+
+signals:
+    void cabtClicked(VkChat *);
+
+private:
+    VkChat *m_chat;
 };
 
 #endif // CHATACTIONBUTTON_H
