28cdef70242e9195ee65804aaca5e4d70bc3bd8d
iEPCBM <rishatk87@gmail.com>
2021-04-05 00:52:41 +0300

    Create dialog confirmation

---
+++ dialogs/dialogchatconfirmation.cpp
@@ -0,0 +1,63 @@
+#include "dialogchatconfirmation.h"
+#include "ui_dialogchatconfirmation.h"
+
+DialogChatConfirmation::DialogChatConfirmation(VkChat *chat, QString token, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::DialogChatConfirmation),
+    m_msgDelivery(token, this)
+{
+    ui->setupUi(this);
+    m_chat = chat;
+    generateCode();
+}
+
+DialogChatConfirmation::~DialogChatConfirmation()
+{
+    delete ui;
+}
+
+QByteArray DialogChatConfirmation::crc32b(QByteArray message)
+{
+    uint32_t byte, crc, mask, len;
+
+    len = message.length();
+    crc = 0xFFFFFFFF;
+
+    for (auto i = 0; --len; i++) {
+       byte = static_cast<uint32_t>(message.at(i));
+       crc = crc ^ byte;
+       for (auto j = 0; j < 8; j++) {
+          mask = -(crc & 1);
+          crc = (crc >> 1) ^ (0xEDB88320 & mask);
+       }
+    }
+    crc = ~crc;
+    QByteArray retVal(reinterpret_cast<char*>(&crc), sizeof(crc));
+    return retVal;
+}
+
+void DialogChatConfirmation::generateCode()
+{
+    m_data2hash = QDate::currentDate().toString(Qt::ISODate)+" "
+            +QTime::currentTime().toString(Qt::ISODateWithMs)
+            +QString::number(VK_API_MULTICHAT_BASE_ID+m_chat->getId())
+            +CRC_SALT;
+    qDebug()<<m_data2hash;
+    QByteArray crcHash = crc32b(m_data2hash.toUtf8()).toHex();
+    qDebug()<<crcHash;
+    m_msgDelivery.sendMessage(VK_API_MULTICHAT_BASE_ID+m_chat->getId(), STR_CONFIRMATION_PREAMBLE+QString::fromUtf8(crcHash));
+}
+
+void DialogChatConfirmation::on_btSendAgain_clicked()
+{
+    generateCode();
+}
+
+void DialogChatConfirmation::on_buttonBox_accepted()
+{
+    QByteArray checkHash = crc32b(m_data2hash.toUtf8()).toHex();
+    if (ui->leCodeInput->text()!=QString::fromUtf8(checkHash)) {
+        QMessageBox::StandardButton bt = QMessageBox::warning(this, "Ошибка", "Код не совпадает", QMessageBox::Retry|QMessageBox::Ok);
+        if (bt==QMessageBox::Retry){this->ui->buttonBox->blockSignals(true);}
+    }
+}
---
+++ dialogs/dialogchatconfirmation.h
@@ -0,0 +1,40 @@
+#ifndef DIALOGCHATCONFIRMATION_H
+#define DIALOGCHATCONFIRMATION_H
+
+#include <QDialog>
+#include <QDebug>
+#include <QTime>
+#include <QDate>
+#include "vkApi/vkchat.h"
+#include "vkApi/vkmessagedelivery.h"
+
+#define CRC_SALT "Руфина, прости меня. Ты мне правда нравишься."
+#define STR_CONFIRMATION_PREAMBLE "Код подтверждения: "
+
+namespace Ui {
+class DialogChatConfirmation;
+}
+
+class DialogChatConfirmation : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DialogChatConfirmation(VkChat *chat, QString token, QWidget *parent = nullptr);
+    ~DialogChatConfirmation();
+
+private slots:
+    void on_btSendAgain_clicked();
+
+    void on_buttonBox_accepted();
+
+private:
+    Ui::DialogChatConfirmation *ui;
+    QByteArray crc32b(QByteArray message);
+    VkChat *m_chat;
+    VkMessageDelivery m_msgDelivery;
+    QString m_data2hash;
+    void generateCode();
+};
+
+#endif // DIALOGCHATCONFIRMATION_H
