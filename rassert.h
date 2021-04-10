3eb9509d987c7e3b54ffdaa4981a1ff3b619fbda
iEPCBM <rishatk87@gmail.com>
2021-04-10 21:35:08 +0300

    Update

--- rassert.h
+++ rassert.h
@@ -10,7 +10,19 @@
                                  "\nВ файле: " + __FILE__ + " (стр.: "+ QString::number(__LINE__) +").\nБудет выполнено действие: " + #EXCEPTION_ACTIONS); \
         EXCEPTION_ACTIONS;}
 
-#define R_ERR_NETWORK(PARENT) \
-
+struct ErrorMessages {
+    static void errorNetwork(QWidget *parent) {
+        QMessageBox::critical(parent, "Проблемы с подключением к Интернету",
+                                 static_cast<QString>("Не удалось подключиться к серверу. Проверьте подключение к Интернету и повторитк попытку."));
+    }
+    static void errorVkApi(QWidget *parent, int errorCode, QString description) {
+        QMessageBox::warning(parent, "Ошибка VK API",
+                                 static_cast<QString>("VK API вернул ошибку.\nКод ошибки: ")+QString::number(errorCode)+"\nОписание ошибки: "+description);
+    }
+    static void errorFileAccess(QWidget *parent, QString filePath) {
+        QMessageBox::critical(parent, "Ошибка доступа к файлу",
+                                 static_cast<QString>("Ошибка доступа к файлу: ")+filePath);
+    }
+};
 
 #endif // RASSERT_H
