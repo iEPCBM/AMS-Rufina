fde4ba85e78c00bc3182a927215790d031442562
iEPCBM <rishatk87@gmail.com>
2021-04-19 11:31:00 +0300

    Create filepaths header

---
+++ filepaths.h
@@ -0,0 +1,16 @@
+#ifndef FILEPATHS_H
+#define FILEPATHS_H
+
+#define FILEPATH_CONFIGS      "./assets/config.xml"
+#define FILEPATH_PROGRAM_ICON "./assets/icons/icon.ico"
+
+#define FILEPATH_QTBASE_RU      ":/langs/qtbase_ru.qm"
+#define FILEPATH_VKAPI_ERR_LIST ":/phrases/errlist.cfg"
+
+#define FILEPATH_ICON_ADD      ":/icons/add"
+#define FILEPATH_ICON_DELETE   ":/icons/delete"
+#define FILEPATH_ICON_EDIT     ":/icons/edit"
+#define FILEPATH_ICON_SETTINGS ":/icons/settings"
+#define FILEPATH_ICON_EXIT     ":/icons/exit"
+
+#endif // FILEPATHS_H
