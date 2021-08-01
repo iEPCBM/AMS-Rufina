ccf1a022a00cb7d8f604647103e8dca1b24f6687
iEPCBM <rishatk87@gmail.com>
2021-08-01 10:46:40 +0500

    Update, add GPL header

--- vkApi/vkchathandler.cpp
+++ vkApi/vkchathandler.cpp
@@ -1,3 +1,24 @@
+/*
+ *  AMS Rufina - automated message delivery program
+ *  Copyright (C) 2021 Rishat D. Kagirov (iEPCBM)
+ *
+ *     This file is part of AMS Rufina.
+ *
+ *  AMS Rufina is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "vkchathandler.h"
 
 VkChatHandler::VkChatHandler(QObject *parent, QString token) :
@@ -40,7 +61,6 @@ void VkChatHandler::responseChatParse()
         return;
     }
     QJsonObject responseObj = json_obj.value("response").toObject();
-    //m_administrators[0] = STR_UNKNOWN;
     if (responseObj.value("count").toInt()==1) {
         QJsonObject item = responseObj.value("items").toArray()[0].toObject();
         if (item.value("peer").toObject().value("type").toString()=="chat") {
--- widgets/chatactionbutton.cpp
+++ widgets/chatactionbutton.cpp
@@ -1,3 +1,24 @@
+/*
+ *  AMS Rufina - automated message delivery program
+ *  Copyright (C) 2021 Rishat D. Kagirov (iEPCBM)
+ *
+ *     This file is part of AMS Rufina.
+ *
+ *  AMS Rufina is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "chatactionbutton.h"
 
 ChatActionButton::ChatActionButton(QString text, VkChat chat, QWidget *parent):QPushButton(text, parent)
